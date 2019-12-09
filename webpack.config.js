1cda3d4a638490816352778c016491a74e790bf2
smohan <smohan@163.com>
2019-12-09 16:38:17 +0800

    修改打包配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,72 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const env = process.env.NODE_ENV || 'development'
-const isDev = env === 'development'
 
-const config = {
-  entry: './src/index.ts',
-  mode: isDev ? 'development' : 'production',
-  output: {
-    path: path.resolve(__dirname, isDev ? './dist/' : './docs/'),
-    filename: isDev ? 'mo.effects.js' : 'mo.effects.[hash:7].js',
-    publicPath: '/',
-    library: 'MoEffects',
-    libraryTarget: 'umd',
-    libraryExport: 'default'
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.js$/,
-        use: [{
-          loader: 'babel-loader'
-        }],
-        exclude: /node_modules/
-      },
-    ],
-  },
-  plugins: [],
-}
-config.plugins.push(
-  new HtmlWebpackPlugin({
-    filename: isDev ? 'index.html' : path.resolve(__dirname, './docs/index.html'),
-    template: path.resolve(__dirname, './examples/index.html'),
-    chunks: true,
-    minify: {
-      removeComments: true,
-      collapseWhitespace: true,
-      removeAttributeQuotes: false,
-      minifyCSS: true,
-      minifyJS: true
+
+
+module.exports = env => {
+  const isDev = env === 'development'
+  const isDoc = env === 'documents'
+  const config = {
+    entry: './src/index.ts',
+    output: {
+      path: path.resolve(__dirname, isDoc ? './docs/' : './dist/'),
+      filename: isDev ? 'mo.effects.js' : 'mo.effects.[hash:7].js',
+      publicPath: '',
+      library: 'MoEffects',
+      libraryTarget: 'umd',
+      libraryExport: 'default'
     },
-  })
-)
-
-// 配置devserver
-if (isDev) {
-  
-  config.devtool = 'inline-source-map'
-  config.devServer = {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9001,
-    hot: true,
-    open: true
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.js$/,
+          use: [{
+            loader: 'babel-loader'
+          }],
+          exclude: /node_modules/
+        },
+      ],
+    },
+    plugins: [],
   }
-}
 
-module.exports = config
+  if (isDoc || isDev) {
+    config.plugins.push(
+      new HtmlWebpackPlugin({
+        filename: isDev ? 'index.html' : path.resolve(__dirname, './docs/index.html'),
+        template: path.resolve(__dirname, './examples/index.html'),
+        chunks: true,
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeAttributeQuotes: false,
+          minifyCSS: true,
+          minifyJS: true
+        },
+      })
+    )
+  }
+
+  // 配置devserver
+  if (isDev) {
+
+    config.devtool = 'inline-source-map'
+    config.devServer = {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9001,
+      hot: true,
+      open: true
+    }
+  }
+
+  return config
+}
